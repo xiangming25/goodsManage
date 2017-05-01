ba25fd184c19f13e899184a7a8640884b36af982
lvxue <1401441809@qq.com>
2017-05-01 17:37:03 +0800

    goods-list json data

--- client/modules/index/js/index.js
+++ client/modules/index/js/index.js
@@ -1,11 +1,37 @@
 ;(function(){
 	require(['/vendor/main.js'],function(){
 		require(['jquery','default'],function($,defaultObj){
-			// alert('yes');
 			$(function(){
-				console.log('加载完成!');
-				console.log(defaultObj.username);
-				console.log(defaultObj.password);
+				$.get("/api/goods_list.json",{
+						page: 1,
+						count: 15
+					},
+					function(data,status){
+						var listData=data.data.list;
+						var tbody=$(".goods-table>tbody");
+						var dataStr = '';
+						listData.forEach(function(item, index){
+							item.isShow = item.isShow ? '是' : '否';
+							dataStr += '<tr>'+
+							'<td>'+item.name+'</td>'+
+							'<td>'+item.unit+'</td>'+
+							'<td><img src="'+item.img+'" alt=""></td>'+
+							'<td>￥'+item.bid+'</td>'+
+							'<td>￥'+item.sellPrice+'</td>'+
+							'<td>'+item.type+'</td>'+
+							'<td>'+item.count+'</td>'+
+							'<td>'+item.time+'</td>'+
+							'<td>'+item.isShow+'</td>'+
+							'<td>'+
+							'	<a href="">查看</a>'+
+							'	<a href="">修改</a>'+
+							'	<a href="">删除</a>'+
+							'</td>'+
+						'</tr>';
+						});
+						tbody.html(dataStr);
+					}
+				)
 			});
 		});
 	});
