113a5f535afed57b2c1f2ebf7321fda34c998660
xiangming <xiangming25@163.com>
2017-06-11 23:17:14 +0800

    完善商品管理sql

--- service/goods.js
+++ service/goods.js
@@ -26,7 +26,21 @@ module.exports = function(pool) {
         connection.query($sql.insert, newParams, function(err, result) {
           if (err) return jsonWrite(res, {code: errorCode.QUERY_ERROR, data: err});
 
-          if (result) return jsonWrite(res, {code: errorCode.SUCCESS, data: 'success'});
+          if (result) jsonWrite(res, {code: errorCode.SUCCESS, data: 'success'});
+          connection.release();
+        });
+      });
+    },
+    // 删除
+    delete: function(req, res, next) {
+      pool.getConnection(function(err, connection) {
+        var params = req.body;
+        connection.query($sql.delete, [params.id], function(err, result) {
+          if (err) {
+            throw err;
+            return jsonWrite(res,err);
+          }
+          if (result) return jsonWrite(res, {code: errorCode.SUCCESS, data: 'delete success'});
           connection.release();
         });
       });
@@ -48,6 +62,37 @@ module.exports = function(pool) {
             jsonWrite(res, {code: errorCode.QUERY_ERROR, data: err});
           }
           if(result) jsonWrite(res,{code: errorCode.SUCCESS, data: result});
+          connection.release();
+        });
+      });
+    },
+    // 查询总数量
+    queryCount: function(req, res, next) {
+      pool.getConnection(function(err, connection) {
+        var userId = req.userInfo.id;
+        connection.query($sql.queryCount,[userId], function(err, result) {
+          if(err) {
+            throw err;
+            return jsonWrite(res, {code: errorCode.QUERY_ERROR, data: '删除失败'});
+          }
+          if (result) jsonWrite(res, {code: errorCode.SUCCESS, data: 'success'});
+        });
+        connection.release();
+      });
+    },
+    // 修改
+    modify: function(req, res, next) {
+      pool.getConnection(function(err, connection) {
+        var params = req.body;
+        params.userId = req.userId.id;
+        connection.query($sql.modify, params, function(err, result) {
+          if(err) {
+            throw err;
+            return jsonWrite(res, {code: errorCode.QUERY_ERROR, data: '删除失败'});
+          }
+          if (result) {
+            jsonWrite(res, {code: errorCode.SUCCESS, data: '删除成功'});
+          }
           connection.release();
         });
       });
--- service/goodsSql.js
+++ service/goodsSql.js
@@ -4,6 +4,9 @@
  */
 var goods = {
   insert: 'insert into goods set ?',
+  delete: 'delete from goods where id = ?',
   queryByPage: 'select * from goods where userId = ? limit ?, ?',
+  queryCount: 'select count(*) from goods where userId = ?',
+  update: 'update goods set name=:name, unit=:unit, image=:image, inPrice=:inPrice, outPrice=:outPrice, type=:type, remainder=:remainder, isShow=:isShow where userId = :userId',
 };
 module.exports = goods;
